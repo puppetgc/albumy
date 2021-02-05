8dce6a2f7828defa69fdb942ac38551cb81b079d
TianMing Xu <78703671@qq.com>
2021-02-05 09:28:01 +0800

    chang name to TianMing Xu

--- albumy/blueprints/ajax.py
+++ albumy/blueprints/ajax.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-    :author: Grey Li (李辉)
+    :author: TianMing Xu (徐天明)
     :url: http://greyli.com
-    :copyright: © 2018 Grey Li <withlihui@gmail.com>
+    :copyright: © 2021 TianMing Xu <78703671@qq.com>
     :license: MIT, see LICENSE for more details.
 """
 from flask import render_template, jsonify, Blueprint
